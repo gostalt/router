63a476a9c975128784afd9ca22b3149b0fe2c4b6
Tom Smith <me@tomm.us>
2020-02-16 20:17:32 +0000

    ADD http verbs

--- router.go
+++ router.go
@@ -44,7 +44,36 @@ func (router *Router) findRoute(path string, method string) (*Route, error) {
 
 // Get defines a new `GET` route on the router, at the given path.
 func (router *Router) Get(path string, handler interface{}) *Route {
-	r := NewRoute(http.MethodGet, path, handler)
+	return router.addRoute(http.MethodGet, path, handler)
+}
+
+// Post defines a new `POST` route on the router, at the given path.
+func (router *Router) Post(path string, handler interface{}) *Route {
+	return router.addRoute(http.MethodPost, path, handler)
+}
+
+// Put defines a new `PUT` route on the router, at the given path.
+func (router *Router) Put(path string, handler interface{}) *Route {
+	return router.addRoute(http.MethodPut, path, handler)
+}
+
+// Patch defines a new `PATCH` route on the router, at the given path.
+func (router *Router) Patch(path string, handler interface{}) *Route {
+	return router.addRoute(http.MethodPatch, path, handler)
+}
+
+// Delete defines a new `DELETE` route on the router, at the given path.
+func (router *Router) Delete(path string, handler interface{}) *Route {
+	return router.addRoute(http.MethodDelete, path, handler)
+}
+
+// Options defines a new `OPTIONS` route on the router, at the given path.
+func (router *Router) Options(path string, handler interface{}) *Route {
+	return router.addRoute(http.MethodOptions, path, handler)
+}
+
+func (router *Router) addRoute(method string, path string, handler interface{}) *Route {
+	r := NewRoute(method, path, handler)
 	router.routes = append(router.routes, r)
 
 	return r
