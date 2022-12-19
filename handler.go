491bd7815c5bc7d1d9f430958042fdeef0b44e04
Thomas Smith <me@tomm.us>
2022-12-19 12:20:53 +0000

    wip

--- handler.go
+++ handler.go
@@ -3,7 +3,32 @@ package router
 import (
 	"fmt"
 	"net/http"
+	"reflect"
 )
+
+var handlerTransformers map[string]interface{} = map[string]interface{}{
+	"func() string":                            new__func_ret_string__Handler,
+	"func(*http.Request) string":               new__func_http_Request_ret_string__Handler,
+	"fmt.Stringer":                             new__Stringer__Handler,
+	"http.HandlerFunc":                         new__http_HandlerFunc__Handler,
+	"func(http.ResponseWriter, *http.Request)": new__func_http_ResponseWriter_http_Request__Handler,
+	"http.Handler":                             new_http_Handler__Handler,
+}
+
+// TODO: Would really rather get the sig dynamically
+func AddHandlerTransformer(sig string, fn interface{}) error {
+	if _, ok := handlerTransformers[sig]; ok {
+		// Handler signature already exists, return an error.
+		return fmt.Errorf("handler signature `%s` already exists, transformer not added", sig)
+	}
+
+	handlerTransformers[sig] = fn
+	return nil
+}
+
+func new_http_Handler__Handler(fn http.Handler) http.Handler {
+	return fn
+}
 
 // new__func_ret_string__Handler returns a valid http.Handler from a function that
 // matches the following signature:
@@ -45,23 +70,19 @@ func new__func_http_ResponseWriter_http_Request__Handler(fn func(http.ResponseWr
 	return http.HandlerFunc(fn)
 }
 
+// buildHandler dynamically creates an http.Handler based on the function signature
+// of the passed in function `fn`.
 func buildHandler(fn interface{}) http.Handler {
-	switch v := fn.(type) {
-	case func(http.ResponseWriter, *http.Request):
-		return new__func_http_ResponseWriter_http_Request__Handler(v)
-	case http.HandlerFunc:
-		return new__http_HandlerFunc__Handler(v)
-	case http.Handler:
-		return v
-	case func() string:
-		return new__func_ret_string__Handler(v)
-	case func(*http.Request) string:
-		return new__func_http_Request_ret_string__Handler(v)
-	case fmt.Stringer:
-		return new__Stringer__Handler(v)
-	default:
-		return makeFailedHandler(v)
-	}
+	// Retrieve the signature of the function. This is the key in the transformer map.
+	a := fmt.Sprintf("%T", fn)
+	handler := reflect.ValueOf(handlerTransformers[a])
+
+	// Reflect the value of `fn` and use it as the argument for the transformer
+	// function. Return the value coerced to an http.Handler.
+	// TODO: Ensure this is safe to do, probably at the point of registration.
+	// Could also `val, ok :=` it here and panic?
+	in := []reflect.Value{reflect.ValueOf(fn)}
+	return handler.Call(in)[0].Interface().(http.Handler)
 }
 
 func makeFailedHandler(handler interface{}) http.HandlerFunc {
